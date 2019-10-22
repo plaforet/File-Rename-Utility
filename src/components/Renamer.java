928c5fba4b67e94017836061371a555923e12e34
plaforet <plaforet@houstonisd.org>
2019-10-22 12:41:19 -0500

    Abstracted out to two methods, rename & renameAll

--- src/components/Renamer.java
+++ src/components/Renamer.java
@@ -6,24 +6,33 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Renamer {
-	public static void rename(ArrayList<String> names, File[] fileList) {
+	/*
+	 * Sorts array of files alphabetically, then iterates through those files,
+	 * skipping hidden files, and renames them based on the ArrayList of names
+	 * (pulled from the CSV).
+	 */
+	public static void renameAll(ArrayList<String> names, File[] fileList) {
 		Arrays.sort(fileList);
 		int skipFile = 0;
 		for (int i = 0; i < fileList.length && i < names.size() + skipFile; i++) {
 			if (fileList[i].isHidden()) {
 				skipFile++;
+			} else {
+				rename(names.get(i - skipFile + 1), fileList[i]);
 			}
-			else {
-				String path = fileList[i].getPath();
-				String extension = path.substring(path.indexOf("."));
-				File f1 = new File(path);
-				System.out.println("f1: " + f1.getPath());
-				File f2 = new File(fileList[i].getParent() + "\\" + names.get(i - skipFile + 1 ) + extension);
-				System.out.println("f2: " + f2.getPath());
-				System.out.println(f1.renameTo(f2));
-			}
-			
 
 		}
+
+	}
+	// Renames one file. Used in the renameAll method.
+	public static void rename(String name, File file) {
+		String path = file.getPath();
+		String extension = path.substring(path.indexOf("."));
+		File f1 = new File(path);
+		System.out.println("f1: " + f1.getPath());
+		File f2 = new File(file.getParent() + "\\" + name + extension);
+		System.out.println("f2: " + f2.getPath());
+		System.out.println(f1.renameTo(f2));
+
 	}
 }
